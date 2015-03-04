9e68256f883425754077f64e286976cd44b1adb1
Baptiste Clavié <clavie.b@gmail.com>
2015-03-04 15:51:37 +0100

    [User] Add the User Object
    This is currently a stub, which will be useful for the hydration when it
    will come (which is coming reaaaally soon)

---
+++ src/User.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the CalendArt package
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @copyright Wisembly
+ * @license   http://www.opensource.org/licenses/MIT-License MIT License
+ */
+
+namespace CalendArt\Adapter\Office365;
+
+use CalendArt\User as BaseUser;
+
+/**
+ * Office365 "User"
+ *
+ * @link https://msdn.microsoft.com/office/office365/APi/complex-types-for-mail-contacts-calendar#EmailAddress
+ * @author Baptiste Clavié <baptiste@wisembly.com>
+ */
+class User extends BaseUser
+{
+}
+
