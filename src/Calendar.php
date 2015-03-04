a75b29d8ad495fdf701178a97fbb26d4320da489
Baptiste Clavié <clavie.b@gmail.com>
2015-03-04 10:57:32 +0100

    [Calendar] Add the Calendar object

---
+++ src/Calendar.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the CalendArt package
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @copyright Wisembly
+ * @license   http://www.opensource.org/licenses/MIT-License MIT License
+ */
+
+namespace CalendArt\Adapter\Office365;
+
+use CalendArt\AbstractCalendar;
+
+class Calendar extends AbstractCalendar
+{
+    /** @var string Calendar's id */
+    private $id;
+
+    /** @var string Calendar's changekey */
+    private $changeKey;
+
+    public function __construct($id, $name = null)
+    {
+        $this->id = $id;
+        parent::__construct($name);
+    }
+
+    /** @return string */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getChangeKey()
+    {
+        return $this->changeKey;
+    }
+}
+
