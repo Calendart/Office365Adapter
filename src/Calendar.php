04a238a19369c5ff03bdacd3d8ab86663557dc76
Baptiste Clavié <clavie.b@gmail.com>
2015-03-06 15:56:57 +0100

    [Calendar] Add a hydrate static method

--- src/Calendar.php
+++ src/Calendar.php
@@ -18,7 +18,12 @@ class Calendar extends AbstractCalendar
     /** @var string Calendar's id */
     private $id;
 
-    /** @var string */
+    /**
+     * @var string Calendar's Etag
+     *
+     * The etag is called a "ChangeKey" in Office365. The real etag
+     * is the ChangeKey prefixed, so it is basically the same thing ?
+     */
     private $etag;
 
     /** @return string */
@@ -30,6 +35,19 @@ class Calendar extends AbstractCalendar
     public function getEtag()
     {
         return $this->etag;
+    }
+
+    public static function hydrate(array $data)
+    {
+        if (!isset($data['Id'], $data['Name'], $data['ChangeKey'])) {
+            throw new InvalidArgumentException(sprintf('Missing at least one of the mandatory properties "Id", "Name", "ChangeKey" ; got ["%s"]', implode('", "', array_keys($data))));
+        }
+
+        $calendar = new static($data['Name']);
+        $calendar->id = $data['Id'];
+        $calendar->etag = $data['ChangeKey'];
+
+        return $calendar;
     }
 }
 
