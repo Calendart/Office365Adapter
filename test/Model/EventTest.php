c29ed0a31e322b64cbd9b7c0ed429e2b2f5a575c
Baptiste Clavié <clavie.b@gmail.com>
2016-04-22 11:46:15 +0200

    Add some tests for null / absent organizer

--- test/Model/EventTest.php
+++ test/Model/EventTest.php
@@ -36,4 +36,63 @@ class EventTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals('2016-04-05T12:00:00+00:00', $event->getStart()->format('c'));
     }
+
+    public function testHydrateNoOwner()
+    {
+        $data = [
+            'id' => 1,
+            'changeKey' => "foo",
+            'createdDateTime' => "2016-03-30T13:16:46.2214781Z",
+            'lastModifiedDateTime' => "2016-03-30T13:16:51.9873431Z",
+            'start' => [
+                "dateTime" => "2016-04-05T12:00:00.0000000",
+                "timeZone" => "UTC",
+            ],
+            'end' => [
+                "dateTime" => "2016-04-05T15:00:00.0000000",
+                "timeZone" => "UTC",
+            ],
+            "recurrence" => null,
+            "isAllDay" => false,
+            "isCancelled" => false,
+            "categories" => [],
+            "importance" => "normal",
+            "showAs" => "busy",
+            "type" => "singleInstance"
+        ];
+
+        $event = Event::hydrate($data);
+
+        $this->assertNull($event->getOwner());
+    }
+
+    public function testHydrateOwnerNull()
+    {
+        $data = [
+            'id' => 1,
+            'changeKey' => "foo",
+            'createdDateTime' => "2016-03-30T13:16:46.2214781Z",
+            'lastModifiedDateTime' => "2016-03-30T13:16:51.9873431Z",
+            'start' => [
+                "dateTime" => "2016-04-05T12:00:00.0000000",
+                "timeZone" => "UTC",
+            ],
+            'end' => [
+                "dateTime" => "2016-04-05T15:00:00.0000000",
+                "timeZone" => "UTC",
+            ],
+            "recurrence" => null,
+            "isAllDay" => false,
+            "isCancelled" => false,
+            "categories" => [],
+            "importance" => "normal",
+            "showAs" => "busy",
+            "type" => "singleInstance",
+            'organizer' => null
+        ];
+
+        $event = Event::hydrate($data);
+
+        $this->assertNull($event->getOwner());
+    }
 }
